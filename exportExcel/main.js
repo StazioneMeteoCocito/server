344ff3f07caa6987160dce9c754654d1767138f4
Stazione Meteo del Liceo Cocito <94685891+StazioneMeteoCocito@users.noreply.github.com>
2022-01-26 23:44:53 +0100

    XLS adjustments

--- exportExcel/main.js
+++ exportExcel/main.js
@@ -1,3 +1,7 @@
+function parseDate(s) {
+  var b = s.split(/\D/);
+  return new Date(b[0], --b[1], b[2]);
+}
 $("#sform").submit(function(e){
     e.preventDefault();
     if($('#sform input:checked').length == 0){
@@ -8,6 +12,11 @@ $("#sform").submit(function(e){
     $("#sform input:checked").each(function(){
          listBox.push($(this).attr("name"));
     });
+ if(parseDate($("[name=end]").val()).getTime()<parseDate($("[name=start]").val()).getTime()){ // swap
+    var start = $("[name=start]").val();
+    $("[name=start]").val($("[name=end]").val())
+    $("[name=end]").val(start);
+ }
  var finalUrl="xls.php?datatypes="+listBox.join(",")+"&start="+$("[name=start]").val()+"&end="+$("[name=end]").val()+"&definition="+$("[name=definition]").val()+"&export="+$("[name=export]").val();
  location.href=finalUrl;
 });
