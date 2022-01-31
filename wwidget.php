f4cac4941a4c912fde97b5d8a962564f831a3a59
MatMasIt <onlinehomework@altervista.org>
2022-01-31 18:33:40 +0100

    SVG switched

--- wwidget.php
+++ wwidget.php
@@ -1,60 +1,38 @@
 <?php
 require("rlib.php");
 //Set the Content Type
-header('Content-type: image/jpeg');
+header('Content-type: image/svg+xml');
 
 function nformat($n){
     return number_format($n,2,",","'");
 }
-$w = 300;
-$h = 500;
-// Create White image
-$img = imagecreate($w,$h);
-
-$bg = imagecolorallocate ( $img, 255, 255, 255 );
-$blue = imagecolorallocate($img, 0, 0, 255);
-$red = imagecolorallocate($img, 255, 0, 0);
-$green = imagecolorallocate($img,0,255,0);
-$pm10 = imagecolorallocate($img,0,96,255);
-imagefilledrectangle($img,0,0,$w,$h,$bg);
-
-// Allocate A Color For The Text
-$black = imagecolorallocate($img, 0, 0, 0);
-
-// Set Path to Font File
-$font_path = 'Roboto-Thin.ttf';
-
-// Print Text On Image
-imagettftext($img, 20, 0, 20, 50, $black, $font_path, "Stazione Meteo");
-
-imageline($img,20,60,200,60,$black);
-
-imagettftext($img,15,0,20,85,$black,$font_path,"Ultimo aggiornamento:\n ".date("d/m/Y H:i:s",file_get_contents("lastContact")));
-
-imagettftext($img, 20,0,20,140,$red,$font_path,"Temperatura:\n ".nformat($dataL["T"])." C");
-
-imageline($img,105,200,100,195,$blue); // accent on Umidità
-
-imagettftext($img,20,0,20,220,$blue,$font_path,"Umidita:\n ".nformat($dataL["H"])." %");
-
-
-imagettftext($img,20,0,20,300,$green,$font_path,"Pressione:\n ".nformat($dataL["P"])." hPa");
-
-
-imagettftext($img,20,0,20,370,$pm10,$font_path,"PM10:\n ".nformat($dataL["PM10"]));
-
-list($w,$h) = getimagesize("umg.jpg");
-$unit = imagecreatefromjpeg("umg.jpg");
-
-if($dataL["PM10"]<100) $left = 90;
-else $left=110;
-imagecopyresized($img,$unit,$left,375,0,0,$w*0.45,$h*0.45,$w,$h);
-
-// Send Image to Browser
-
-
-imagejpeg($img);
-
-// Clear Memory
-imagedestroy($img);
 ?>
+<svg width="300" height="500" xmlns="http://www.w3.org/2000/svg">
+ <title>Stazione Meteo</title>
+
+ <g>
+  <title>Stazione Meteo</title>
+  <text xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_1" y="32" x="47" stroke-width="0" stroke="#000" fill="#000000">Stazione Meteo</text>
+  <line id="svg_2" y2="46" x2="304.00169" y1="46" x1="0.00001" stroke="#000" fill="none"/>
+  <text transform="matrix(0.758496, 0, 0, 0.85076, 73.966, 13.1995)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" stroke-width="0" id="svg_3" y="75.1229" x="-54.00953" stroke="#000" fill="#000000">Ultimo aggiornamento:</text>
+  <text transform="matrix(0.758496, 0, 0, 0.85076, 73.966, 13.1995)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" stroke-width="0" id="svg_4" y="115.08717" x="-35.55195" stroke="#000" fill="#000000"><?php echo date("d/m/Y H:i:s",file_get_contents("lastContact")); ?></text>
+  <line id="svg_5" y2="127" x2="302.00169" y1="127" x1="-1.99999" stroke="#000" fill="none"/>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_7" y="176.36275" x="26.00056" stroke="#ff0000" fill="#ff2e00">Temperatura</text>
+  <line id="svg_8" y2="204" x2="300.46625" y1="204" x1="-4" stroke="#000" fill="none"/>
+  <line id="svg_9" y2="522.00508" x2="146" y1="128" x1="146" stroke="#000" fill="none"/>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_10" y="178.68137" x="258.50917" stroke="#ff0000" fill="#ff2e00"><?php echo nformat($dataL["T"]); ?> °C</text>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_12" y="263.3112" x="54.50162" stroke="#0400ff" fill="#0800ff">Umidità</text>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_13" y="263.3112" x="258.50916" stroke="#0400ff" fill="#0800ff"><?php echo nformat($dataL["H"]); ?> %</text>
+  <line id="svg_14" y2="276" x2="320.07608" y1="276" x1="-2" stroke="#0400ff" fill="none"/>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_15" y="345.6224" x="41.00112" stroke="#19ff00" fill="#00ff2e">Pressione</text>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_16" y="343.30378" x="242.00856" stroke="#19ff00" fill="#00ff2e"><?php echo nformat($dataL["P"]); ?>hPa</text>
+  <line id="svg_17" y2="344" x2="311.01447" y1="344" x1="0" stroke="#000000" fill="none"/>
+  <line id="svg_18" y2="485" x2="311" y1="485" x1="312" stroke="#000000" fill="none"/>
+  <line id="svg_19" y2="401" x2="385" y1="401" x1="380" stroke="#000000" fill="none"/>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_20" y="426.7743" x="74.00234" stroke="#00ffff" fill="#00ffff">PM10</text>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_21" y="425.61499" x="258.50916" stroke="#00ffff" fill="#00ffff"><?php echo nformat($dataL["PM10"]); ?> µg/m³</text>
+  <path id="svg_22" d="m-6,415l342.21046,0" opacity="undefined" stroke="#000000" fill="none"/>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_23" y="512.56344" x="72.50228" stroke="#d000ff" fill="#e500ff">PM2,5</text>
+  <text transform="matrix(0.666642, 0, 0, 0.86258, 3.45887, 21.1058)" xml:space="preserve" text-anchor="start" font-family="'Roboto Mono'" font-size="24" id="svg_24" y="513.72276" x="248.00878" stroke="#d000ff" fill="#e500ff"><?php echo nformat($dataL["PM25"]); ?> µg/m³</text>
+ </g>
+</svg>
